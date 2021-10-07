8b60e835661ac3952258742cdda43d31c261df66
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-07 14:49:16 +0200

    Paragraph

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
-
+    $text = 'Un nuovo, sorprendente salto di qualità per le fotocamere. Un display così reattivo che anche i gesti di sempre ti stupiranno. Il chip per smartphone più veloce al mondo. Robustezza eccezionale. E autonomia senza precedenti.
+    Entra nel mondo Pro.'
 ?>
